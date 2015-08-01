9d4e33ae412dd602bae7aeb66ca6a7cf9711ed84
Luis <josecastillocs@gmail.com>
2015-08-01 00:05:06 -0500

    Included a method using typedef and called it from main()

--- typedefExample.c
+++ typedefExample.c
@@ -11,6 +11,16 @@
  	int weightInKilos;
  } Person;
 
+ /* Thanks to the new typedef  we can pass a Person to another function.
+  * We can add a function named bodyMassIndex() that accepts a Person
+  * as a parameter and caoculates BMI. Then, we can update main() to 
+  * call this function. */
+
+float bodyMassIndex(Person p)
+{
+	return p.weightInKilos / (p.heightInMeters * p.heightInMeters);
+}
+
  int main(int argc, const char * argv[])
  {
  	Person Jose;
@@ -21,10 +31,18 @@
 	Aaron.heightInMeters = 1.97;
  	Aaron.weightInKilos = 87;
 
- 	printf("Using typef struc to define our data type: \n");
- 	printf("Jose is %.2f meters tall\n", Jose.heightInMeters);
- 	printf("Jose weighs %d kilograms\n", Jose.weightInKilos);
+ 	printf("Using typef struct to define our data type: \n");
+ 	//printf("Jose is %.2f meters tall\n", Jose.heightInMeters);
+ 	//printf("Jose weighs %d kilograms\n", Jose.weightInKilos);
+ 	//printf("\nAaron is %.2f meters tall\n", Aaron.heightInMeters);
+ 	//printf("Aaron weighs %d kilograms\n", Aaron.weightInKilos);
 
- 	printf("\nAaron is %.2f meters tall\n", Aaron.heightInMeters);
- 	printf("Aaron weighs %d kilograms\n", Aaron.weightInKilos);
+ 	float bmi;
+ 	bmi = bodyMassIndex(Jose);
+ 	printf("Jose has a BMI of %.2f\n", bmi);
+
+ 	bmi = bodyMassIndex(Aaron);
+ 	printf("Aaron has a BMI of %.2f\n", bmi);
+
+ 	return 0;
  }
