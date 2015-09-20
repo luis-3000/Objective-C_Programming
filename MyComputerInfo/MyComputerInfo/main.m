b51d4f4de475c09efa8f41414499ca4aee6d8c24
Jose Luis Castillo <joseluiscastillo@Joses-MacBook-Pro.local>
2015-09-20 18:29:21 -0500

    Simple exercise to get my computer's information using NSHost and NSString

---
+++ MyComputerInfo/MyComputerInfo/main.m
@@ -0,0 +1,27 @@
+//
+//  main.m
+//  MyComputerInfo
+//
+//  Created by Jose Luis Castillo on 9/20/15.
+//  Copyright © 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        //My computer's information
+        //Create a NSHost object/instance for this computer
+        NSHost *myComputerCompleteInfo = [NSHost currentHost];
+        
+        //Get the name for this computer by passing to the instance of NSHost
+        //just created above the localizedName message/method which returns
+        //a pointer to an instance of NSString
+        NSString *myComputerName = [myComputerCompleteInfo localizedName];
+        
+        //print the name of this computer
+        NSLog(@"The name of this computer is: %@", myComputerName);
+    }
+    return 0;
+}
